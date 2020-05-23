e25842830360bf76b8c98b3b9f818830bce38e5e
YCFELIX\yauch <ycfelix7@gmail.com>
2020-05-23 22:24:33 +0800

    fully done.

--- graphics.py
+++ graphics.py
@@ -1,36 +1,24 @@
 import numpy as np
-
+import time
 '''
-tuples of arr_item, bar matrix,bar height, start_x,end_x
+tuples of arr_item, x1,y1,x2,y2
 '''
 
-def get_height(arr_item,min_val,max_val,step_height):
-    return int(((arr_item-min_val)/(max_val-min_val)+1)*step_height)
+def get_height(arr_item,min_val,max_val,h):
+    return int((min(max_val-min_val,arr_item+5-min_val)/(max_val-min_val))*h)
 
-def make_bar(barwidth,barheight):
-    result=np.zeros(((barheight,barwidth)))
-    result[5:barheight-5,5:barwidth-5]=255
-    return result
-
-def render_graph(h,w,bars):
-    result = np.zeros((h, w))
-    for item in bars:
-        e, bar,barheight,start,end=item
-        result[0:barheight, start:end] = bar
-    return result
-
-
-def init_graphics(h,w,arr):
+def init_graphics(h,w,arr,canvas):
     bars=[]
     arr=np.array(arr)
-    result=np.zeros((h,w))
     min_item=np.min(arr)
     max_item=np.max(arr)
-    step_height=h//(max_item-min_item)
     barwidth = w // arr.shape[0]
     for i,e in enumerate(arr):
-        barheight=get_height(e,min_item,max_item,step_height)
-        bar=make_bar(barwidth,barheight)
-        result[0:barheight,i*barwidth:(i+1)*barwidth]=bar
-        bars.append([e,bar,barheight,i*barwidth,(i+1)*barwidth])
-    return result,bars
+        barheight=get_height(e,min_item,max_item,h)
+        bar=canvas.create_rectangle(i*barwidth+5,h-barheight+5,(i+1)*barwidth-5,h-5,fill='black')
+        text_x=(i*barwidth+5+(i+1)*barwidth-5)//2
+        text_y=h+5
+        text=canvas.create_text(text_x, text_y, fill="green", font="Times 12 italic bold",
+                           text=str(e))
+        bars.append([e,bar,text,i*barwidth+5,h-barheight+5,(i+1)*barwidth-5,h-5])
+    return bars
--- main.py
+++ main.py
@@ -1,77 +1,173 @@
 from tkinter import messagebox
 import tkinter as tk
-import PIL.Image, PIL.ImageTk
 import threading
 import time
-import numpy as np
 import graphics
-import cv2
+import timeit
+import re
+import random
+import sys
+
 window = tk.Tk()
 window.title('Sorting visualization')
 
-OPTIONS = [
-"Bubble sort",
-"Merge sort",
-"Quick sort"
+ALGO_OPTIONS = [
+    "Bubble sort",
+    "Heap sort",
+    "Quick sort"
 ]
-# cv_img=cv2.imread("test.jpg",cv2.IMREAD_GRAYSCALE)
-cv_img=np.zeros((400,600))
-height, width=cv_img.shape
-image_on_canvas=None
-photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_img))
+SPEED_OPTIONS = ["1","2","3","4"]
+
+detail = tk.StringVar()
+running = tk.StringVar()
+speed_var = tk.StringVar(window)
+sort_algo = tk.StringVar(window)
+default_arr = [random.randint(-50, 50) for i in range(19)]
+default = tk.StringVar(window, value="{}".format(default_arr))
+
+speed_var.set(SPEED_OPTIONS[0])
+pause = tk.IntVar()
+sort_algo.set(ALGO_OPTIONS[0])
+
+height, width = 400, 600
+expand = 30
 
 def sort_click():
-    global entry
-    algo=sort_algo.get()
-    # arr=eval(entry.get())
+    global entry, sort_btn
+    sort_btn["state"] = tk.DISABLED
+    running.set(str(0))
+    algo = sort_algo.get()
+    arr = re.findall("^\[[\d|,|\s|-]*\]$", entry.get())
+    if not arr:
+        messagebox.showinfo("Wrong input!", '''
+           your input is wrong!''')
+        return
+    else:
+        arr = eval(arr[0])
+    detail.set("Sorting algorithm:{} \n Expected output:{}".format(algo, sorted(arr)))
     def sort_by_time():
-        global photo, canvas
-        cv_img[:,:],bars = graphics.init_graphics(height,width,[7,6,5,4,3,2,1])
-        photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_img))
-        canvas.create_image(0, 0, image=photo, anchor=tk.NW)
-        bubble_sort(bars)
+        global photo,canvas
+        canvas.delete("all")
+        bars = graphics.init_graphics(height, width, arr, canvas)
+        if algo == ALGO_OPTIONS[0]:
+            bubble_sort(bars, canvas)
+        elif algo == ALGO_OPTIONS[1]:
+            heap_sort(bars, canvas)
+        elif algo == ALGO_OPTIONS[2]:
+            start = timeit.default_timer()
+            quick_sort(bars, 0, len(bars) - 1, start, canvas)
+        sort_btn["state"] = tk.NORMAL
+    running_thread = threading.Thread(target=sort_by_time)
+    running_thread.start()
 
-    t=threading.Thread(target=sort_by_time)
-    t.start()
 
 '''
 play animation here
 '''
-def swap(arr,i,j):
-    global photo, canvas
-    e1, bar1, barheight1, start1, end1=arr[i]
-    e2, bar2, barheight2, start2, end2=arr[j]
-    for dis in range(start2-start1):
-        arr[i][3] = start1+dis
-        arr[i][4] = end1+dis
-        arr[j][3] = start2-dis
-        arr[j][4] = end2-dis
-        cv_img[:, :]=graphics.render_graph(height, width,arr)
-        photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
-        canvas.create_image(0, 0, image=photo, anchor=tk.NW)
-    arr[i],arr[j]=arr[j],arr[i]
-    cv_img[:, :] = graphics.render_graph(height, width, arr)
-    photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
-    canvas.create_image(0, 0, image=photo, anchor=tk.NW)
 
-def bubble_sort(bars):
+
+def swap(arr, i, j, canvas, start):
+    fast = int(speed_var.get())
+    ei, bari, texti, xi1, yi1, xi2, yi2 = arr[i]
+    ej, barj, textj, xj1, yj1, xj2, yj2 = arr[j]
+    if ei == ej:
+        return
+    canvas.itemconfig(bari, fill='red')
+    canvas.itemconfig(barj, fill='red')
+    canvas.itemconfig(texti, fill='red')
+    canvas.itemconfig(textj, fill='red')
+    for dis in range(abs(xj1 - xi1)):
+        while pause.get():
+            time.sleep(1)
+        if xj1 > xi1:
+            canvas.move(bari, 1, 0)
+            canvas.move(barj, -1, 0)
+            canvas.move(texti, 1, 0)
+            canvas.move(textj, -1, 0)
+        else:
+            canvas.move(bari, -1, 0)
+            canvas.move(barj, 1, 0)
+            canvas.move(texti, -1, 0)
+            canvas.move(textj, 1, 0)
+        time.sleep((1 / fast) / abs(xj1 - xi1))
+        running.set("Running time: " + "{0:.2f} s".format(timeit.default_timer() - start))
+    arr[i][3], arr[j][3] = arr[j][3], arr[i][3]
+    arr[i][5], arr[j][5] = arr[j][5], arr[i][5]
+    arr[i], arr[j] = arr[j], arr[i]
+    canvas.itemconfig(texti, fill='green')
+    canvas.itemconfig(textj, fill='green')
+    canvas.itemconfig(bari, fill='black')
+    canvas.itemconfig(barj, fill='black')
+
+
+def partition(arr, low, high, canvas, start):
+    i = (low - 1)
+    pivot = arr[high][0]
+    for j in range(low, high):
+        if arr[j][0] < pivot:
+            i = i + 1
+            swap(arr, i, j, canvas, start)
+    swap(arr, i + 1, high, canvas, start)
+    return (i + 1)
+
+
+def quick_sort(arr, low, high, start, canvas):
+    if low < high:
+        pi = partition(arr, low, high, canvas, start)
+        quick_sort(arr, low, pi - 1, start, canvas)
+        quick_sort(arr, pi + 1, high, start, canvas)
+
+
+
+def bubble_sort(bars, canvas):
+    start = timeit.default_timer()
     for i in range(len(bars)):
         for j in range(len(bars)):
-            if j<=i:
+            if j <= i:
                 continue
-            if bars[j][0]<bars[i][0]:
-                swap(bars,i,j)
+            if bars[j][0] < bars[i][0]:
+                swap(bars, i, j, canvas, start)
 
 
 def help_click():
     messagebox.showinfo("Sample input", '''
     python array format \n Example: [1,3,7,9,5,4,2,6]''')
 
-sort_algo=tk.StringVar(window)
-sort_algo.set(OPTIONS[0])
+
+def heapify(arr, n, i, canvas, start):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i][0] < arr[l][0]:
+        largest = l
+    if r < n and arr[largest][0] < arr[r][0]:
+        largest = r
+    if largest != i:
+        swap(arr, i, largest, canvas, start)
+        heapify(arr, n, largest, canvas, start)
+
+
+def heap_sort(arr, canvas):
+    start = timeit.default_timer()
+    n = len(arr)
+    for i in range(n, -1, -1):
+        heapify(arr, n, i, canvas, start)
+    for i in range(n - 1, 0, -1):
+        swap(arr, i, 0, canvas, start)
+        heapify(arr, i, 0, canvas, start)
+
 
 header_label = tk.Label(window, text='Enter a customized array')
 header_label.pack()
+
+
+def close_program():
+    window.destroy()
+    sys.exit()
+
+
+tk.Button(window, text='close program', command=close_program).pack()
 
 height_frame = tk.Frame(window)
 height_frame.pack(side=tk.TOP)
@@ -79,22 +175,36 @@ height_frame.pack(side=tk.TOP)
 height_label = tk.Label(height_frame, text='array of integer')
 height_label.pack(side=tk.LEFT)
 
-entry = tk.Entry(height_frame,width=60)
+entry = tk.Entry(height_frame, width=60, textvariable=default)
 entry.pack(side=tk.LEFT)
 
-help_btn = tk.Button(height_frame, text='help',command=help_click)
+help_btn = tk.Button(height_frame, text='help', command=help_click)
 help_btn.pack(side=tk.LEFT)
 
-menu = tk.OptionMenu(window, sort_algo, *OPTIONS)
+menu = tk.OptionMenu(window, sort_algo, *ALGO_OPTIONS)
 menu.pack()
 
-sort_btn = tk.Button(window, text='sort',command=sort_click)
-sort_btn.pack()
+button_area = tk.Frame(window)
+button_area.pack(side=tk.TOP)
 
-canvas = tk.Canvas(window, width = width, height = height)
+speed_label = tk.Label(button_area, text='Animation speed')
+speed_label.pack(side=tk.LEFT)
+
+speed = tk.OptionMenu(button_area, speed_var, *SPEED_OPTIONS)
+speed.pack(side=tk.LEFT)
+
+sort_btn = tk.Button(button_area, text='sort', command=sort_click)
+sort_btn.pack(side=tk.LEFT)
+
+tk.Checkbutton(button_area, text="Pause", variable=pause).pack(side=tk.LEFT)
+
+detail_label = tk.Label(window, textvariable=detail)
+detail_label.pack()
+
+running_label = tk.Label(window, textvariable=running)
+running_label.pack()
+
+canvas = tk.Canvas(window, width=width + expand, height=height + expand)
 canvas.pack()
-
-canvas.create_image(0, 0, image=photo, anchor=tk.NW)
-
 
 window.mainloop()
